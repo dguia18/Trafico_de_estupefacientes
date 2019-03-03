97882a47404aaf32bfaf2e43c340ed6a12f6435f
dguia18 <dguia@unicesar.edu.co>
2019-03-02 20:14:47 -0500

    cruce de autos

--- Modelos/Assets/Scripts/movimientoCarro.cs
+++ Modelos/Assets/Scripts/movimientoCarro.cs
@@ -11,7 +11,39 @@ public class movimientoCarro : MonoBehaviour
 
     }
     void Update (){
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
         transform.Translate(0, 0, speed * Time.deltaTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation, speed * Time.deltaTime);
+
+        //if (this.transform.position.x <= 194.5f && this.transform.position.x >= 192f)
+        //{
+        //    int opcionMovimiento = 0;
+        //    opcionMovimiento = Random.Range(0, 3);
+        //    if (opcionMovimiento == 2)
+        //    {
+        //        transform.Rotate(new Vector3(0f, 225f, 0f) * Time.deltaTime);
+        //    }
+        //}
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Girar1")
+        {            
+                transform.Rotate(new Vector3(0f, 90f, 0f));
+        }        
+        if (collision.gameObject.name == "Girar")
+        {
+            int opcionMovimiento = 0;
+            opcionMovimiento = Random.Range(0, 3);
+            Debug.Log(opcionMovimiento);
+            if (opcionMovimiento == 2)
+                transform.Rotate(new Vector3(0f, 90f, 0f));
+        }        
     }
 }
