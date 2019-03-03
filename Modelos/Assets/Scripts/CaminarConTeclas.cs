729200393dbad0b196df805d03ed277947ff5a62
dguia18 <dguia@unicesar.edu.co>
2019-03-03 07:01:58 -0500

    Cruce de carros

---
+++ Modelos/Assets/Scripts/CaminarConTeclas.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaminarConTeclas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float deltaRotation=30f;
+    public float deltaMovement = 10f;
+
+
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        Movement();
+    }
+
+    void Rotate() {
+        if (Input.GetKey(KeyCode.Q))
+            transform.Rotate(new Vector3(0f, -deltaRotation, 0f) * Time.deltaTime);
+        else
+            if (Input.GetKey(KeyCode.E))
+            transform.Rotate(new Vector3(0f, deltaRotation, 0f) * Time.deltaTime);
+
+    }
+
+    void Movement() {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * deltaMovement * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * deltaMovement * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * deltaMovement * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * deltaMovement * Time.deltaTime);
+    }
+}
--- Modelos/Assets/Scripts/carsInstantate.cs
+++ Modelos/Assets/Scripts/carsInstantate.cs
@@ -22,7 +22,7 @@ public class carsInstantate : MonoBehaviour
     IEnumerator ThrowStones()
     {
         // Initial delay
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
 
         while (true)
         {
