1796f7ffd652819d7992ef8dfcedc2b256eff03f
Ivan Contreras <ivancontry.13@gmail.com>
2019-03-05 06:51:02 -0500

    sockets

---
+++ Modelos/Assets/Scripts/Sockets/Unit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+    public int unitID;
+    public bool isPlayersUnit;
+
+    void Update()
+    {
+        if ((Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) && isPlayersUnit)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (PlayerControl.map.GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
+            {
+                float x = hit.point.x;
+                float y = hit.point.y;
+                float z = hit.point.z;
+                GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(hit.point);
+                PlayerControl.client.Send("Moving|" + unitID + "|" + x + "|" + y + "|" + z + "|");
+            }
+        }
+    }
+
+    public void MoveTo(Vector3 pos)
+    {
+        GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(pos);
+    }
+}
---
+++ Modelos/Assets/Scripts/carsInstantateSockets.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carsInstantateSockets : MonoBehaviour
+{
+    public float instanceRate = 15f;
+    private float nextInstance = 0.0f;
+    public GameObject taxi;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+       
+    }
+    public void recibirCarro(string name, string emisor)
+    {
+        GameObject prefab = Resources.Load(name) as GameObject;
+        Debug.Log(name);
+        if (Time.time > nextInstance)
+        {
+            nextInstance = Time.time + instanceRate;
+            if (emisor== "emisor25_1")
+            {
+                GameObject cubo1 = GameObject.Find("receptorGlorieta1");
+                Instantiate(prefab.gameObject, cubo1.transform.position, cubo1.transform.rotation);
+                Debug.Log(cubo1.transform.position.x);
+                Debug.Log(cubo1.transform.position.y);
+                Debug.Log(cubo1.transform.position.z);
+            }
+            if (emisor == "emisor25_2")
+            {
+                GameObject cubo2 = GameObject.Find("receptorGlorieta2");
+                Instantiate(prefab.gameObject, cubo2.transform.position, cubo2.transform.rotation);
+                Debug.Log(cubo2.transform.position.x);
+                Debug.Log(cubo2.transform.position.y);
+                Debug.Log(cubo2.transform.position.z);
+            }
+        }
+    }
+}
